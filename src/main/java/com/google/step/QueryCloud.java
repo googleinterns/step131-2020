1fecbadbd4751695b024cd8830931e30ca23855f
Darren Thompson <dothompson@google.com>
2020-06-16 22:56:41 +0000

    Initial commit

---
+++ src/main/java/com/google/step/QueryCloud.java
@@ -0,0 +1,20 @@
+package com.google.step;
+
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/query-cloud")
+public class QueryCloud extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+  }
+
+
+}
---
+++ src/main/java/com/google/step/QueryDatastore.java
@@ -0,0 +1,20 @@
+package com.google.step;
+
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/query-datastore")
+public class QueryDatastore extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+  }
+
+
+}

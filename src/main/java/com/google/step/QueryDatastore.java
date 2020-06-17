7870dd01e96c13722af538f18255dd15e4222f29
Darren Thompson <darren.thompson200@gmail.com>
2020-06-17 03:46:57 +0000

    Fixed some file problems

--- src/main/java/com/google/step/QueryDatastore.java
+++ src/main/java/com/google/step/QueryDatastore.java
@@ -10,11 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/query-datastore")
 public class QueryDatastore extends HttpServlet {
+    private final String PROJECT_ID = System.getenv("PROJECT_ID");
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+    }
 
-  }
-
-
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+    }
 }

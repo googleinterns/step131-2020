54e38bcedc520bf8d8e4a56131e394a94ced5148
Gabrielle Germanson <gbgermanson@google.com>
2020-06-25 20:03:11 +0000

    Cron, MapImage class, save to Datastore

---
+++ src/main/java/com/google/step/MapImage.java
@@ -0,0 +1,115 @@
+ 
+package com.google.step;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.cloud.storage.Blob.BlobSourceOption;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageException;
+import com.google.cloud.storage.StorageOptions;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
+import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
+import com.google.api.services.drive.Drive.Files;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class representing a map snapshot image and its metadata.
+ */
+public class MapImage {
+
+  /** Snapshot's longitude coordinate. */
+  private double longitude;
+
+  /** Snapshot's latitude coordinate. */
+  private double latitude;
+
+  /** Snapshot's cityName coordinate. */
+  private String cityName;
+
+  /** Snapshot's zoom coordinate. */
+  private int zoom;
+
+  /** Month snapshot was took. */
+  private int month;
+
+  /** Year snapshot was took. */
+  private int year;
+
+  /** Exact time snapshot was took. */
+  private int timeStamp;
+
+  /** Represents attributes of a MapImage unique instance (New_York_5x_06_2020.png) and is a name. */
+  private String objectID;  
+
+  public MapImage(double longitude, double latitude, String cityName, int zoom, int month, int year, int timeStamp) {
+      this.longitude = longitude;
+      this.latitude = latitude;
+      this.cityName = cityName;
+      this.zoom = zoom;
+      this.month = month;
+      this.year = year;
+      this.timeStamp = timeStamp;
+  }
+  
+  /** 
+  * Create name based off of attributes: city name, zoom level, month, year.
+  */
+  public String setObjectID() {
+      return (cityName + "_" + zoom  + "x_" + month + "_" + year + ".png");
+  }
+
+  public double getLongitude() {
+      return longitude;
+  }
+
+  public double getLatitude() {
+      return latitude;
+  }
+  public String getCityName() {
+      return cityName;
+  }
+
+  public int getZoom() {
+      return zoom;
+  }
+
+  public int getMonth() {
+      return longitude;
+  }
+
+  public int getYear() {
+      return year;
+  }  
+
+  public int getTimeStamp() {
+      return timeStamp;
+  }
+
+  public String getObjectID() {
+      return objectID;
+  }    
+}
---
+++ src/main/java/com/google/step/MapImageDataStore.java
@@ -0,0 +1,68 @@
+ package com.google.step;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.cloud.storage.Blob.BlobSourceOption;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageException;
+import com.google.cloud.storage.StorageOptions;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
+import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
+import com.google.api.services.drive.Drive.Files;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+// TODO: Tests
+
+@WebServlet("/save-datastore")
+public class MapImageDataStore extends HttpServlet {
+
+    /** Stores MapImage object into Datastore. */
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+        // TODO: Send MapImage objects here. OR combine this function into SaveImages.java servlet
+        MapImage mapImage = null;
+
+        // Creat new entity of kind MapImage and look-up key is MapImage's object name.
+        Entity mapImageEntity = new Entity("MapImage", mapImage.getObjectID());
+
+        mapImageEntity.setProperty("Longitude", mapImage.getLongitude());
+        mapImageEntity.setProperty("Latitude", mapImage.getLongitude());
+        mapImageEntity.setProperty("City Name", mapImage.getCityName());
+        mapImageEntity.setProperty("Zoom", mapImage.getZoom());
+        mapImageEntity.setProperty("Month", mapImage.getMonth());
+        mapImageEntity.setProperty("Year", mapImage.getYear());
+        mapImageEntity.setProperty("Time Stamp", mapImage.getTimeStamp());
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(mapImageEntity);
+
+        // TODO: adjust redirect location
+        response.sendRedirect("/index.html");
+    }
+
+}

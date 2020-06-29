e962a6b71ef115e7afb705257bf7b917f7bca84b
cgregori <cgregori@fordham.edu>
2020-06-29 22:58:10 +0000

    Basic code for backend of adding a new location

---
+++ src/main/java/com/google/step/AddLocation.java
@@ -0,0 +1,30 @@
+package com.google.step;
+
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.storage.Blob.BlobSourceOption;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/add-location")
+public class AddLocation extends HttpServlet {
+    private final String PROJECT_ID = System.getenv("PROJECT_ID");
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        double latitude = request.getParameter("latitude");
+        double longitude = request.getParameter("longitude");
+        String cityName = request.getParameter("cityName");
+
+        // A new "TrackedLocation" entity should be added as well.
+    }
+}
+
+
+

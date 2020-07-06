aba0b3875e9a3bfa697d35eddcc2d38eaf0cae36
cgregori <cgregori@fordham.edu>
2020-07-06 18:51:43 +0000

    Load image metadata in separate servlet

---
+++ src/main/java/com/google/step/FrontendQueryDatastore.java
@@ -0,0 +1,132 @@
+package com.google.step;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.KeyFactory;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.QueryResults;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.StructuredQuery.CompositeFilter;
+import com.google.cloud.datastore.StructuredQuery.PropertyFilter;
+import com.google.cloud.datastore.StructuredQuery.Filter;
+import java.io.IOException;
+import com.google.gson.Gson;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.DataOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.util.List;
+import java.util.ArrayList;
+
+/***
+    This servlet retrieves the mapImage metadata (location, zoom level, etc.) from Datastore.
+    A POST request gets the parameters from a form and returns the MapImages to be displayed.
+***/
+@WebServlet("/frontend-query-datastore")
+public class FrontendQueryDatastore extends HttpServlet {
+    private final String PROJECT_ID = System.getenv("PROJECT_ID");
+
+    /** Get form parameters and query Datastore to get objectIDs based on those parameters*/
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService(); // Authorized Datastore service.
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("mapImage"); // Used to create keys later.
+
+        String zoomStr = request.getParameter("Zoom Level");
+        String city = request.getParameter("City");
+        String monthStr = request.getParameter("Month");
+        String yearStr = request.getParameter("yearInput");  
+
+        // Add the appropriate filters according to the form input.
+        CompositeFilter compositeFilter = buildCompositeFilter(zoomStr, city, monthStr, yearStr);
+
+        // Build the query for Datastore.
+        Query<Entity> query = Query.newEntityQueryBuilder()
+            .setKind("mapImage")
+            .setFilter(compositeFilter)
+            .build();
+
+        // Add all the mapEntities that matched the filter
+        QueryResults<Entity> resultList = datastore.run(query);
+        ArrayList<MapImage> mapImages = entitiesToMapImages(resultList);
+
+        // Send the mapImages back to app.html
+        Gson gson = new Gson();
+        URL url = new URL("/app.html");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoInput(true);
+        con.setDoOutput(true);
+        String data = gson.toJson(mapImages);
+        try(DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+            wr.write(data.getBytes(StandardCharsets.UTF_8));
+        }
+    }
+
+    private CompositeFilter buildCompositeFilter(String zoomStr, String city, String monthStr, String yearStr) {
+        // Check for empty values from the form and build filters for user-input values.
+        ArrayList<Filter> filters = new ArrayList<>();
+        try {
+            int zoom = Integer.parseInt(zoomStr);
+            filters.add(PropertyFilter.eq("zoom", zoom));
+        } catch (NumberFormatException e) {
+            // TODO: log and handle error.
+        }
+        try {
+            int month = Integer.parseInt(monthStr);
+            filters.add(PropertyFilter.eq("month", month));
+        } catch (NumberFormatException e) {
+            // TODO: log and handle error.
+        }
+        try {
+            int year = Integer.parseInt(yearStr);
+            filters.add(PropertyFilter.eq("year", year));
+        } catch (NumberFormatException e) {
+            // TODO: log and handle error.
+        }
+        if (!city.equals("")) {
+            filters.add(PropertyFilter.eq("cityName", city));
+        }
+
+        // Construct the CompositeFilter.
+        CompositeFilter compositeFilter = null;
+        for (Filter filter : filters) {
+            compositeFilter = CompositeFilter.and(filter);
+        }
+        return compositeFilter;
+    }
+
+    private ArrayList<MapImage> entitiesToMapImages(QueryResults<Entity> resultList) {
+        ArrayList<MapImage> resultMapImages = new ArrayList<>();
+        while (resultList.hasNext()) {  // While we still have data
+            resultMapImages.add(entityToMapImage(resultList.next())); // Add the MapImage to the List
+        }
+        return resultMapImages;
+    }
+
+    private MapImage entityToMapImage(Entity entity) {
+        /* 
+        *   NOTE: entity.get"Type" (i.e. entity.getDouble) will return either DatastoreException
+        *   if the property doesn't exist, or a ClassCastException if the value is the wrong type
+        */
+
+        double latitude = entity.getDouble("latitude");
+        double longitude = entity.getDouble("longitude");
+        int zoom = (int) entity.getLong("zoom");
+        String cityName = entity.getString("cityName");
+        int month = (int) entity.getLong("month");
+        int year = (int) entity.getLong("year");
+        String timeStamp = entity.getString("timeStamp");
+
+        MapImage mapImage = new MapImage(longitude, latitude, cityName, zoom, month, year, timeStamp);
+        mapImage.setObjectID();
+        return mapImage;
+    }
+}
--- src/main/java/com/google/step/QueryDatastore.java
+++ src/main/java/com/google/step/QueryDatastore.java
@@ -23,7 +23,6 @@ import java.util.ArrayList;
 /***
     This servlet retrieves the mapImage metadata (location, zoom level, etc.) from Datastore.
     A GET request gets TrackedLocations so request URLs can be constructed and sent to Static Maps.
-    A POST request gets the parameters from a form and returns the MapImages to be displayed.
 ***/
 @WebServlet("/query-datastore")
 public class QueryDatastore extends HttpServlet {
@@ -57,103 +56,11 @@ public class QueryDatastore extends HttpServlet {
         con.setRequestMethod("POST");
         con.setRequestProperty("Content-Type", "application/json");
         con.setRequestProperty("Accept", "application/json");
+
         con.setDoOutput(true);
         String data = gson.toJson(mapImages);
         try(DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
             wr.write(data.getBytes(StandardCharsets.UTF_8));
         }
-    }
-
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        /* TODO: Set up a way to use both the API and the package for Datastore.
-        *   Currently, I cannot create a Filter object without using the Datastore package.
-        *   However, using the package only and not the API causes too many headaches
-        *   and makes the code unable to compile without a significant overhaul.
-        *   The current idea is to use the API for doGet and the package for doPost.
-        *   Future issues are converting the package "Entity" to the API "Entity".
-        *   This causes the code to not compile. Currently the code does not compile.
-        */
-
-        String zoomStr = request.getParameter("Zoom Level");
-        String city = request.getParameter("City");
-        String monthStr = request.getParameter("Month");
-        String yearStr = request.getParameter("yearInput");
-
-        int zoom = null;
-        int month = null;
-        int year = null;    
-
-        try {
-            zoom = Integer.parseInt(zoomStr);
-        } catch (NumberFormatException e) {
-            // TODO: handle error
-        }
-        try {
-            month = Integer.parseInt(monthStr);
-        } catch (NumberFormatException e) {
-            // TODO: handle error
-        }
-        try {
-            year = Integer.parseInt(yearStr);
-        } catch (NumberFormatException e) {
-            // TODO: handle error
-        }
-
-        // TODO: This code does not work when using the Datastore API. Only w/ the Datastore package
-        ArrayList<Filter> filters = new ArrayList<>();
-        CompositeFilter compositeFilter = null;
-        if(zoom != null) {
-            filters.add(PropertyFilter.eq("zoom", zoom));
-        }
-        if (month != null) {
-            filters.add(PropertyFilter.eq("month", month));
-        }
-        if (year != null) {
-            filters.add(PropertyFilter.eq("year", year));
-        }
-        if(!city.equals("")) {
-            filters.add(PropertyFilter.eq("cityName", city));
-        }
-
-        for(Filter filter : filters) {
-            compositeFilter = CompositeFilter.and(filter);
-        }
-
-        Query<Entity> query = Query.newEntityQueryBuilder()
-            .setKind("mapImage")
-            .setFilter(compositeFilter)
-            .build();
-
-        // Add all the mapEntities that matched the filter
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        PreparedQuery results = datastore.prepare(query);
-        ArrayList<MapImage> mapImages = new ArrayList<>();
-        for(Entity entity : results.asIterable()) {
-            double latitude = (double) entity.getProperty("latitude");
-            double longitude = (double) entity.getProperty("longitude");
-            int zoom = (int) entity.getProperty("zoom");
-            String cityName = (String) entity.getProperty("cityName");
-            int month = (int) entity.getProperty("month");
-            int year = (int) entity.getProperty("year");
-            String timeStamp = (String) entity.getProperty("timeStamp");
-
-            MapImage mapImage = new MapImage(longitude, latitude, cityName, zoom, month, year, timeStamp);
-            mapImages.add(mapImage);
-        }
-
-        // Send the mapImages back to app.html
-        Gson gson = new Gson();
-        URL url = new URL("/app.html");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("Accept", "application/json");
-        con.setDoOutput(true);
-        String data = gson.toJson(mapImages);
-        try(DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
-            wr.write(data.getBytes(StandardCharsets.UTF_8));
-        }
-
     }
 }

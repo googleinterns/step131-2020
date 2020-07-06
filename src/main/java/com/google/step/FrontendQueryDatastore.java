a88d9211f7cc97df8dd24f9b5c0e8360d12b9782
cgregori <cgregori@fordham.edu>
2020-07-06 21:11:26 +0000

    Change Datastore capitalization error

--- src/main/java/com/google/step/FrontendQueryDatastore.java
+++ src/main/java/com/google/step/FrontendQueryDatastore.java
@@ -35,7 +35,7 @@ public class FrontendQueryDatastore extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Datastore datastore = DatastoreOptions.getDefaultInstance().getService(); // Authorized Datastore service.
-        KeyFactory keyFactory = datastore.newKeyFactory().setKind("mapImage"); // Used to create keys later.
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("MapImage"); // Used to create keys later.
 
         String zoomStr = request.getParameter("zoomLevel");
         String city = request.getParameter("city");
@@ -47,13 +47,13 @@ public class FrontendQueryDatastore extends HttpServlet {
 
         // Build the query for Datastore.
         Query<Entity> query = Query.newEntityQueryBuilder()
-            .setKind("mapImage")
+            .setKind("MapImage")
             .setFilter(compositeFilter)
             .build();
 
         // Add all the mapEntities that matched the filter
         QueryResults<Entity> resultList = datastore.run(query);
-        ArrayList<MapImage> Images = new ArrayList<>();
+        ArrayList<MapImage> mapImages = new ArrayList<>();
         try {
             mapImages = entitiesToMapImages(resultList);
         } catch (ClassCastException e) {

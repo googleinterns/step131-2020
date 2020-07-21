6a3a50ad630cfac3e5add5bb0e56e74e77ac3f83
Gabrielle Germanson <gbgermanson@google.com>
2020-07-21 20:34:16 +0000

    Styling 1

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // Attempted code for titles within selectors
-    // var div = document.createElement("div");
+    // var div = document.createElement('div');
     // div.className = 'dropdown-title';
     // div.attr('style','font-size:11px');
     // div.attr('style','text-transform: uppercase');
@@ -21,7 +21,7 @@ $(document).ready(function() {
             $('#locations').val(locations[0]);
             $('.selectpicker').selectpicker('refresh');
         });
-    
+
     fetch('/query-cloud').then((response) => response.json()).then((array) => {
         // array '{}' on page load: request made before form submission
         // array not '{}' when request is made after form submission.
@@ -29,7 +29,7 @@ $(document).ready(function() {
             for (let i = 0; i < array.length; i++) {
                 const url = array[i].url;
                 // TODO: create entire image list structure
-                $('#requested-images').append(`<li><img src='${url}'></li>`);
+                $('#requested-images').append(`<li><img src='${url}'></li>`);            
             }
         }
     });
@@ -40,30 +40,32 @@ $(document).ready(function() {
     fetch('/start-save-drive');
 });
 
+/** Load date capability for form. */
 function loadDateRange() {
-    let startDate = moment().subtract(1, 'month');
-    let endDate = moment();
+    const startDate = moment().subtract(1, 'month');
+    const endDate = moment();
 
+    /** Helper date function. */
     function callback(start, end) {
         $('#request-form').submit((eventObj) => {
             // Add date range as hidden values to form.
-            $('<input />').attr("type", "hidden")
-                .attr("id", "startDateId")
-                .attr("value", start.unix())
-                .attr("name", "startDate")
+            $('<input />').attr('type', 'hidden')
+                .attr('id', 'startDateId')
+                .attr('value', start.unix())
+                .attr('name', 'startDate')
                 .appendTo('#request-form');
-            $('<input />').attr("type", "hidden")
-                .attr("id", "endDateId")
-                .attr("value", end.unix())
-                .attr("name", "endDate")
+            $('<input />').attr('type', 'hidden')
+                .attr('id', 'endDateId')
+                .attr('value', end.unix())
+                .attr('name', 'endDate')
                 .appendTo('#request-form');
             return true;
         });
     }
 
-    $('input[name="dateFilter"]').daterangepicker({
-        "showDropdowns": true,
-         ranges: {
+    $('input[name='dateFilter']').daterangepicker({
+        'showDropdowns': true,
+        ranges: {
             'A month ago': [moment().subtract(1, 'month'), moment()],
             '3 months ago': [moment().subtract(3, 'month'), moment()],
             '6 months ago': [moment().subtract(6, 'month'), moment()],
@@ -71,23 +73,25 @@ function loadDateRange() {
             '18 months ago': [moment().subtract(18, 'month'), moment()],
             '2 years ago': [moment().subtract(2, 'year'), moment()]
         },
-        "linkedCalendars": false,
-        "alwaysShowCalendars": true,
-        "startDate": startDate,
-        "endDate": endDate,
-        "minDate": "07/01/2020"
+        'linkedCalendars': false,
+        'alwaysShowCalendars': true,
+        'startDate': startDate,
+        'endDate': endDate,
+        'minDate': '07/01/2020'
     }, callback);
 }
 
-/** TODO: Once the html elements for the images are finalized, this code will be used to download the images in a zip folder */
-// JSZipUtils.getBinaryContent("[imageURL]", function (err, data) {
+/** TODO: Once the html elements for the images are finalized, this code 
+*   will be used to download the images in a zip folder 
+*/
+// JSZipUtils.getBinaryContent('[imageURL]', function (err, data) {
 //    if(err) {
 //       throw err; // or handle the error
 //    }
 //    var zip = new JSZip();
-//    zip.file("img.png", data, {binary:true});zip.generateAsync({type:"blob"})
+//    zip.file('img.png', data, {binary:true});zip.generateAsync({type:'blob'})
 //     .then(function (blob) {
-//         saveAs(blob, "hello.zip");
+//         saveAs(blob, 'hello.zip');
 //     });
 // });
 

93bea103a92604b5a7231e93e3d7a2591e0d0e4b
cgregori <cgregori@fordham.edu>
2020-07-28 20:42:09 +0000

    Boilerplate code for testing

---
+++ src/test/java/com/google/step/FrontendQueryDatastoreTest.java
@@ -0,0 +1,70 @@
+package com.google.step;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.appengine.api.datastore.dev.LocalDatastoreService.AutoIdAllocationPolicy;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+
+@RunWith(JUnit4.class)
+public final class FrontendQueryDatastoreTest {
+    private DatastoreService datastore;
+    private FrontendQueryDatastore frontendQueryDatastore;
+
+    // Local Datastore for testing purposes.
+    private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+//    MapImage MAP_IMAGE_A = new MapImage(77.2176496, 28.6282961, "Delhi", 10, 7, 2020, 1594038988);
+//    MapImage MAP_IMAGE_B = new MapImage(-0.0911334, 51.5054466, "London", 7, 7, 2020, 1594038988);
+//    MapImage TRACKED_LOCATION_MAP_IMAGE_A = new MapImage(77.2176496, 28.6282961, "Delhi", 10);
+//    MapImage TRACKED_LOCATION_MAP_IMAGE_B = new MapImage(-0.0911334, 51.5054466, "London", 7);
+//    Entity TRACKED_LOCATION_ENTITY_A;
+//    Entity TRACKED_LOCATION_ENTITY_B;
+
+    @Before
+    public void setUp() {
+        //NOTE: Instantiating Entities before performing helper.setUp() causes the API error.
+        helper.setUp();
+        datastore = DatastoreServiceFactory.getDatastoreService();
+
+        // Set up Datastore entities.
+//        TRACKED_LOCATION_ENTITY_A = new Entity("TrackedLocation");
+//        TRACKED_LOCATION_ENTITY_B = new Entity("TrackedLocation");
+//        TRACKED_LOCATION_ENTITY_A.setProperty("latitude", 28.6282961);
+//        TRACKED_LOCATION_ENTITY_A.setProperty("longitude", 77.2176496);
+//        TRACKED_LOCATION_ENTITY_A.setProperty("cityName", "Delhi");
+//        TRACKED_LOCATION_ENTITY_B.setProperty("latitude", 51.5054466);
+//        TRACKED_LOCATION_ENTITY_B.setProperty("longitude", -0.0911334);
+//        TRACKED_LOCATION_ENTITY_B.setProperty("cityName", "London");
+//
+//        backendQueryDatastore = new BackendQueryDatastore();
+    }
+
+    @After
+    public void done() {
+        helper.tearDown();
+    }
+
+    @Test
+    public void temp() {
+        System.out.println("I do nothing!");
+    }
+}
